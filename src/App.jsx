f5e9f59fc42527e2d5eece9ab56a28b05139be9a
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-17 18:02:56 +0530

    refactor: remove Card component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import Card from './components/Card.jsx'
+
 
 const App = () => {
-  return (
-    <div
-      className='bg-[#07070cf4] h-screen p-10'>
-      <div className='flex flex-col items-center gap-5'>
-        <Card name="IronMan" />
-        <Card name="Spiderman" />
-        <Card name="Thor" />
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className={"font-3xl bold underline"}>Hello</h1>
+        </div>
+    )
 }
-
 export default App
