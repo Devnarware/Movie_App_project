0203b0835b2cde5cd0b794aaf48d18780ba1e9c7
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-19 17:16:24 +0530

    fix: correct spelling of trending section and add movie poster images

--- src/App.jsx
+++ src/App.jsx
@@ -101,13 +101,14 @@ const App = () => {
                 </header>
 
                 {trendingMovies.length > 0 && (
-                    <section className="treiding">
+                    <section className="trending">
                         <h2>Trending Movies</h2>
 
                         <ul>
                             {trendingMovies.map((movie, index) =>(
                                 <li key={movie.$id}>
                                     <p>{index + 1}</p>
+                                    <img src={movie.poster_url} alt={movie.title} />
                                 </li>
                                 )
                             )}
