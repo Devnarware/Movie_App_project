02ee3c9ac6fd314af4a67856f81d14fc9be995dd
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-19 16:05:36 +0530

    fix: correct casing of 'searchterm' in updateSearchCount function

--- src/appwrite.js
+++ src/appwrite.js
@@ -13,7 +13,7 @@ const database = new Databases(client)
 export const updateSearchCount = async (searchTerm, movie) =>{
     try{
         const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-            Query.equal('searchTerm', searchTerm)
+            Query.equal('searchterm', searchTerm)
         ])
 
         if(result.documents.length > 0){
@@ -24,10 +24,10 @@ export const updateSearchCount = async (searchTerm, movie) =>{
             })
         }else {
             await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique, {
-                searchTerm,
+                searchterm: searchTerm,
                 count: 1,
                 movie_id: movie.id,
-                poster_url: `https://image.tmdb.org/t/p/w500${poster_path}`
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
             })
         }
     }catch(error){
