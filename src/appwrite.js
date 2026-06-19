cb6ec2352bec024ad2588238ffc1dad90e27efe7
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-19 15:43:47 +0530

    feat: implement updateSearchCount function to track search term usage in Appwrite

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,9 +1,31 @@
+import { Client, Databases, Query } from "appwrite"
+
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTIONS_ID
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID
 
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID)
 
+const database = new Databases(client)
 
-export const updateSearchCount = async () =>{
-    console.log(DATABASE_ID, COLLECTION_ID, PROJECT_ID);
+export const updateSearchCount = async (searchTerm, movie) =>{
+    try{
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', searchTerm)
+        ])
+
+        if(result.documents.length > 0){
+            const doc = result.documents[0]
+
+            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+                count: doc.count + 1
+            })
+        }else {
+            
+        }
+    }catch(error){
+        console.log(error);
+    }
 }
